4ea89f81f5e60cc08b847be7537915e0ebc44d7b
Genevieve Zenla <gen@nyble.dev>
2022-10-11 17:20:20 -0500

    Conversion mapping!

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,7 @@ fn main() {
 
 struct World {
 	smitten: Smitten,
-	rock: Vec<Entity>,
-	paper: Vec<Entity>,
-	scissors: Vec<Entity>,
+	things: Vec<Entity>,
 	earlier: Option<Instant>,
 	revolve: RevolvingRandom,
 }
@@ -42,41 +40,29 @@ impl World {
 		.take(50)
 		.collect();
 
-		let mut rock = vec![];
-		let mut paper = vec![];
-		let mut scissors = vec![];
+		let mut things = vec![];
 
 		for (id, (position, direction)) in positions_directions.into_iter().enumerate() {
 			let kind_idx = thread_rng().gen_range(0..3);
 
-			match kind_idx {
-				0 => rock.push(Entity {
-					id,
-					kind: Kind::Rock,
-					position,
-					direction,
-				}),
-				1 => paper.push(Entity {
-					id,
-					kind: Kind::Paper,
-					position,
-					direction,
-				}),
-				2 => scissors.push(Entity {
-					id,
-					kind: Kind::Scissors,
-					position,
-					direction,
-				}),
+			let kind = match kind_idx {
+				0 => Kind::Rock,
+				1 => Kind::Paper,
+				2 => Kind::Scissors,
 				_ => unreachable!(),
 			};
+
+			things.push(Entity {
+				id,
+				kind,
+				position,
+				direction,
+			})
 		}
 
 		Self {
 			smitten,
-			rock,
-			paper,
-			scissors,
+			things,
 			earlier: None,
 			revolve: RevolvingRandom::new(),
 		}
@@ -104,23 +90,20 @@ impl World {
 		// and I need a &mut on that, too, and the compiler doesn't know that
 		// the former only uses rock, paper, and scissors. It's protecting us
 		// here. From the crime of two mutable borrows
-		self.rock
-			.iter_mut()
-			.chain(self.paper.iter_mut())
-			.chain(self.scissors.iter_mut())
-			.for_each(|e| {
-				let jitter = 0.05;
-				e.position += Vec2::new(
-					self.revolve.range(-jitter, jitter),
-					self.revolve.range(-jitter, jitter),
-				)
-			});
+		self.things.iter_mut().for_each(|e| {
+			let jitter = 0.05;
+			e.position += Vec2::new(
+				self.revolve.range(-jitter, jitter),
+				self.revolve.range(-jitter, jitter),
+			)
+		});
 
-		self.foreach_entities_mut(|e| {
+		self.things.iter_mut().for_each(|e| {
 			e.position += e.direction * Entity::SPEED * delta.as_secs_f32();
 		});
 
 		self.collide_walls();
+		self.collide_entities();
 	}
 
 	const ENTITY_DIM: Vec2 = Vec2 { x: 1.0, y: 1.0 };
@@ -128,18 +111,15 @@ impl World {
 	pub fn draw(&self) {
 		self.smitten.clear();
 
-		// I really didn't need to make a macro here. Could've made a weird little function sintead, but, too late
-		macro_rules! draw_vec {
-			($iterator:expr, $draw:ident) => {
-				$iterator
-					.iter()
-					.for_each(|ent| self.smitten.rect(ent.position, Self::ENTITY_DIM, $draw));
+		self.things.iter().for_each(|ent| {
+			let draw = match ent.kind {
+				Kind::Rock => ROCK_COLOR,
+				Kind::Paper => PAPER_COLOR,
+				Kind::Scissors => SCISSORS_COLOR,
 			};
-		}
 
-		draw_vec!(self.rock, ROCK_COLOR);
-		draw_vec!(self.paper, PAPER_COLOR);
-		draw_vec!(self.scissors, SCISSORS_COLOR);
+			self.smitten.rect(ent.position, Self::ENTITY_DIM, draw)
+		});
 
 		self.smitten.swap();
 	}
@@ -153,22 +133,11 @@ impl World {
 		)
 	}
 
-	fn foreach_entities_mut<F>(&mut self, f: F)
-	where
-		F: FnMut(&mut Entity),
-	{
-		self.rock
-			.iter_mut()
-			.chain(self.paper.iter_mut())
-			.chain(self.scissors.iter_mut())
-			.for_each(f);
-	}
-
 	fn collide_walls(&mut self) {
 		let walls = self.smitten.screen_murs() / 2;
 		let entdim = Self::ENTITY_DIM.x / 2.0;
 
-		self.foreach_entities_mut(|ent| {
+		self.things.iter_mut().for_each(|ent| {
 			if ent.position.x + entdim > walls.x {
 				ent.position.x = walls.x - entdim;
 				ent.direction.x *= -1.0;
@@ -186,6 +155,43 @@ impl World {
 			}
 		})
 	}
+
+	fn collide_entities(&mut self) {
+		let mut seen = vec![];
+
+		loop {
+			let mut thing = match self.things.pop() {
+				Some(thing) => thing,
+				None => break,
+			};
+
+			for th in self.things.iter_mut() {
+				if thing.collides_with(th) {
+					match thing.kind {
+						Kind::Rock => match th.kind {
+							Kind::Rock => (),
+							Kind::Paper => thing.kind = Kind::Paper,
+							Kind::Scissors => th.kind = Kind::Rock,
+						},
+						Kind::Paper => match th.kind {
+							Kind::Rock => th.kind = Kind::Paper,
+							Kind::Paper => (),
+							Kind::Scissors => thing.kind = Kind::Scissors,
+						},
+						Kind::Scissors => match th.kind {
+							Kind::Rock => thing.kind = Kind::Rock,
+							Kind::Paper => th.kind = Kind::Scissors,
+							Kind::Scissors => (),
+						},
+					}
+				}
+			}
+
+			seen.push(thing);
+		}
+
+		self.things = seen;
+	}
 }
 
 type EntityId = usize;
@@ -199,7 +205,12 @@ struct Entity {
 }
 
 impl Entity {
-	const SPEED: f32 = 1.0;
+	const SPEED: f32 = 2.0;
+
+	pub fn collides_with(&self, other: &Entity) -> bool {
+		//gen- Why is this not ENTITY_DIM.x * 2?
+		self.position.distance_with(other.position) < World::ENTITY_DIM.x
+	}
 }
 
 enum Kind {
