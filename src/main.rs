6d50d6082b6c2a775e920c02a9d2a10311f15487
Genevieve Zenla <gen@nyble.dev>
2022-10-11 16:23:42 -0500

    Slowly! go slowly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use rand::{thread_rng, Rng};
 use smitten::{self, Color, Smitten, Vec2};
 
@@ -19,21 +21,27 @@ struct World {
 	rock: Vec<Entity>,
 	paper: Vec<Entity>,
 	scissors: Vec<Entity>,
+	earlier: Option<Instant>,
 }
 
 impl World {
 	pub fn new() -> Self {
 		let smitten = Smitten::new((1280, 960), "Rock Paper Scissors", 24);
 
-		let positions: Vec<Vec2> = std::iter::repeat_with(|| Self::random_position(&smitten))
-			.take(50)
-			.collect();
+		let positions_directions: Vec<(Vec2, Vec2)> = std::iter::repeat_with(|| {
+			(
+				Self::random_position(&smitten),
+				Self::random_position(&smitten).normalize(),
+			)
+		})
+		.take(50)
+		.collect();
 
 		let mut rock = vec![];
 		let mut paper = vec![];
 		let mut scissors = vec![];
 
-		for (id, position) in positions.into_iter().enumerate() {
+		for (id, (position, direction)) in positions_directions.into_iter().enumerate() {
 			let kind_idx = thread_rng().gen_range(0..3);
 
 			match kind_idx {
@@ -41,16 +49,19 @@ impl World {
 					id,
 					kind: Kind::Rock,
 					position,
+					direction,
 				}),
 				1 => paper.push(Entity {
 					id,
 					kind: Kind::Paper,
 					position,
+					direction,
 				}),
 				2 => scissors.push(Entity {
 					id,
 					kind: Kind::Scissors,
 					position,
+					direction,
 				}),
 				_ => unreachable!(),
 			};
@@ -61,11 +72,32 @@ impl World {
 			rock,
 			paper,
 			scissors,
+			earlier: None,
 		}
 	}
 
 	pub fn tick(&mut self) {
 		let _events = self.smitten.events();
+
+		// We can't
+		let delta = match self.earlier {
+			None => {
+				self.earlier = Some(Instant::now());
+				return;
+			}
+			Some(earlier) => {
+				let now = Instant::now();
+				let delta = now.duration_since(earlier);
+				self.earlier = Some(now);
+				delta
+			}
+		};
+
+		self.foreach_entities_mut(|e| {
+			e.position += e.direction * Entity::SPEED * delta.as_secs_f32();
+		});
+
+		self.collide_walls();
 	}
 
 	const ENTITY_DIM: Vec2 = Vec2 { x: 1.0, y: 1.0 };
@@ -97,6 +129,40 @@ impl World {
 			thread_rng().gen_range(-murs.y..murs.y),
 		)
 	}
+
+	fn foreach_entities_mut<F>(&mut self, f: F)
+	where
+		F: FnMut(&mut Entity),
+	{
+		self.rock
+			.iter_mut()
+			.chain(self.paper.iter_mut())
+			.chain(self.scissors.iter_mut())
+			.for_each(f);
+	}
+
+	fn collide_walls(&mut self) {
+		let walls = self.smitten.screen_murs() / 2;
+		let entdim = Self::ENTITY_DIM.x / 2.0;
+
+		self.foreach_entities_mut(|ent| {
+			if ent.position.x + entdim > walls.x {
+				ent.position.x = walls.x - entdim;
+				ent.direction.x *= -1.0;
+			} else if ent.position.x - entdim < -walls.x {
+				ent.position.x = -walls.x + entdim;
+				ent.direction.x *= -1.0;
+			}
+
+			if ent.position.y + entdim > walls.y {
+				ent.position.y = walls.y - entdim;
+				ent.direction.y *= -1.0;
+			} else if ent.position.y - entdim < -walls.y {
+				ent.position.y = -walls.y + entdim;
+				ent.direction.y *= -1.0;
+			}
+		})
+	}
 }
 
 type EntityId = usize;
@@ -105,6 +171,12 @@ struct Entity {
 	id: EntityId,
 	kind: Kind,
 	position: Vec2,
+	/// Movement direction. Where is it going?
+	direction: Vec2,
+}
+
+impl Entity {
+	const SPEED: f32 = 1.0;
 }
 
 enum Kind {
